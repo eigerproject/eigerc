a94a10bd95fbe6244f6240d1158dca972051accd
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2026-02-26 15:10:43 +0400

    Tests

---
+++ tests/runner.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+
+#include <cstdio>
+#include <string>
+
+std::string runInterpreter(const std::string& file) {
+    std::string command = "./eigerc -s " + file;
+
+    FILE* pipe = popen(command.c_str(), "r");
+    if (!pipe) return "";
+
+    char buffer[128];
+    std::string result = "";
+
+    while (!feof(pipe)) {
+        if (fgets(buffer, 128, pipe) != NULL) result += buffer;
+    }
+
+    pclose(pipe);
+
+    return result;
+}
+
+std::string readFile(const std::string& file) {
+    std::string content = "";
+    FILE* filePtr = fopen(file.c_str(), "r");
+    if (!filePtr) return content;
+
+    char buffer[128];
+    while (!feof(filePtr)) {
+        if (fgets(buffer, 128, filePtr) != NULL) content += buffer;
+    }
+    fclose(filePtr);
+    return content;
+}
+
+#define TEST_FILE(suite, name, path)                          \
+    TEST(suite, name) {                                       \
+        auto content = runInterpreter(path);                  \
+        auto expected = readFile(std::string(path) + ".out"); \
+        EXPECT_EQ(content, expected);                         \
+    }
+
+TEST_FILE(IntegrationTests, Arithmetic, "tests/arithmetic.eig");
